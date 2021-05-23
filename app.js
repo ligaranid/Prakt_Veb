c5f503d1fab1936ec79dda62e4704837393bffd0
Dinara <gilmanovadi75@gmail.com>
2021-05-23 17:25:43 +0300

    Настроили содержимое вкладки

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
-var main = function () {
-	"use strict";
-	var makeTabActive = function (tabNumber) { //сконструируем селектор из tabNubmer
-		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-		$(".tabs span").removeClass("active");
-		$(tabSelector).addClass("active");
-	};
-	$(".tabs a:nth-child(1)").on("click", function () {
-		makeTabActive(1);
-		return false;
-	});
-	$(".tabs a:nth-child(2)").on("click", function () {
-		makeTabActive(2);
-		return false;
-	});
-	$(".tabs a:nth-child(3)").on("click", function () {
-		makeTabActive(3);
-		return false;
-	});
-	};
-		
+"use strict";
+
+var toDos = [
+    "Закончить писать эту книгу",
+    "Вывести Грейси на прогулку в парк",
+    "Ответить на электронные письма",
+    "Подготовиться к лекции в понедельник",
+    "Обновить несколько новых задач",
+    "Купить продукты"
+];
+
+$("document").ready(function () {
+
+    $(".tabs a span").toArray().forEach(function (element) {
+        $(element).on("click", function () {
+            var $element = $(element);
+            $(".tabs a span").removeClass("active");
+            $(element).addClass("active");
+            $("main .content").empty();
+
+            if ($element.parent().is(":nth-child(1)")) {
+                for (var i = toDos.length - 1; i > -1; i--) {
+                    $(".content").append($("<li>").text(toDos[i]));
+                }
+            }
+
+            else if ($element.parent().is(":nth-child(2)")) {
+                toDos.forEach(function (todo) {
+                    $(".content").append($("<li>").text(todo));
+                });
+            }
+
+            else if ($element.parent().is(":nth-child(3)")) {
+                $(".content").append(
+                    '<input type="text" class="vvod">' +
+                    '<button class="knopka">Add</button>'
+                );
+                var newTD;
+                $('.knopka').on('click', function () {
+                    newTD = $('.vvod').val();
+                    if (newTD != '') {
+                        toDos.push(newTD);
+                        alert('Задание добавлено');
+                        $('.vvod').val("");
+                        $(".comments").append("<p>" + newTD + "</p>");
+                    }
+                })
+            }
+            return false;
+        })
+    })
+
+    $(".tabs a:first-child span").trigger("click");
+
+})
